b0700cd90dd25896d097a701d25c2a7c3b6fd3d3
1210449106 <3519943@qq.com>
2022-03-11 18:16:14 +0800

    Changes

--- TestNetWork/src/com/wry/login02/TestClient.java
+++ TestNetWork/src/com/wry/login02/TestClient.java
@@ -12,27 +12,56 @@ import java.util.Scanner;
  */
 public class TestClient {//客户端
     //这是一个main方法，是程序的入口
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 //        1.创建套接字:必须指定服务器ip和端口号
-        Socket s = new Socket("192.168.68.35",8888);
-//        录入用户名密码
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入账号");
-        String username = sc.next();
-        System.out.println("请输入密码");
-        String password = sc.next();
+        Socket s = null;
+        OutputStream os = null;
+        ObjectOutputStream oos = null;
+        InputStream is =null;
+        DataInputStream dis = null;
+        try {
+            s = new Socket("192.168.68.35",8888);
+            //        录入用户名密码
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入账号");
+            String username = sc.next();
+            System.out.println("请输入密码");
+            String password = sc.next();
 //        2.向外发送数据==》输出流
-        OutputStream os = s.getOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(os);
+            os = s.getOutputStream();
+            oos = new ObjectOutputStream(os);
 //        利用OutputStream就可以向外发送数据了，但是没有直接发送String的方法
 //        所以我们又在OutputStream外面套了一个处理流：DataOutPutStream
-        oos.writeObject(new User(username,password));
+            oos.writeObject(new User(username,password));
 //        接收服务端的回话 -->利用流
-        InputStream is = s.getInputStream();
-        DataInputStream dis = new DataInputStream(is);
-        System.out.println(dis.readUTF());
-//        关闭流 网络资源
-        oos.close();
-        s.close();
+            is = s.getInputStream();
+            dis = new DataInputStream(is);
+            System.out.println(dis.readUTF());
+        } catch (IOException e) {
+            System.out.println("服务器连接失败");;
+        }finally {
+            //        关闭流 网络资源
+            try {
+                if (oos != null){
+                    oos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (dis != null) {
+                    dis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (s != null) {
+                    s.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- TestNetWork/src/com/wry/login02/TestServer.java
+++ TestNetWork/src/com/wry/login02/TestServer.java
@@ -12,30 +12,74 @@ import java.net.Socket;
  */
 public class TestServer {
     //这是一个main方法，是程序的入口
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args){
+        ServerSocket ss = null;
+        InputStream is = null;
+        ObjectInputStream ois = null;
+        OutputStream os = null;
+        DataOutputStream dos = null;
+        try {
 //        1.创建套接字
-        ServerSocket ss = new ServerSocket(8888);
+            ss = new ServerSocket(8888);
 //        2.等客户端的信息
-        Socket s = ss.accept();//阻塞方法：等待接收客户端的数据，什么时候收到数据，什么时候继续向下执行
+            Socket s = ss.accept();//阻塞方法：等待接收客户端的数据，什么时候收到数据，什么时候继续向下执行
 //        accept()返回值为一个Socket，这个Socket是客户端的Socket
 //        3.接到Socket之后，客户端和服务器才真正可以通信
-        InputStream is = s.getInputStream();
-        ObjectInputStream ois = new ObjectInputStream(is);
+            is = s.getInputStream();
+            ois = new ObjectInputStream(is);
 //        4.读取数据
-        User user = (User)(ois.readObject());
+            User user = (User)(ois.readObject());
 //        5.准备输出流
-        OutputStream os = s.getOutputStream();
-        DataOutputStream dos = new DataOutputStream(os);
+            os = s.getOutputStream();
+            dos = new DataOutputStream(os);
 //        5,开始判断并返回结果
-        if ("123456".equals(user.getUsername())&&"123456".equals(user.getPassword())){
-            dos.writeUTF("登陆成功");
-        }
-        else {
-            dos.writeUTF("登陆失败");
-        }
+            if ("123456".equals(user.getUsername())&&"123456".equals(user.getPassword())){
+                dos.writeUTF("登陆成功");
+            }
+            else {
+                dos.writeUTF("登陆失败");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
 //        关闭流+网络资源
-        ois.close();
-        dos.close();
-        ss.close();
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (os != null) {
+                    os.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (dos != null) {
+                    dos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (ss != null) {
+                    ss.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
